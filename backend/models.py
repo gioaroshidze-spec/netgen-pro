198a0990ae838595d6626a521cdd090bbc056ae6
leftcatt <leftcat9@gmail.com>
2026-04-15 13:48:44 +0400

    Added models.py where i defined the network device

---
+++ backend/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class NetworkDevice(Base):
+    # This is the actual name of the table inside the SQLite database
+    __tablename__ = "network_devices"
+
+    # Columns for our table
+    id = Column(Integer, primary_key=True, index=True)
+    hostname = Column(String, unique=True, index=True)
+    ip_address = Column(String, unique=True, index=True)
+    device_type = Column(String) # e.g., 'cisco_ios', 'arista_eos'
+    username = Column(String)
+
+    # Note: We will handle passwords and ssh keys securely later!
+
